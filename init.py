9ad3a34eb5c29a7c677e6e1614c854193a9bcc1d
gnavihs <shivang0007@gmail.com>
2017-02-12 17:30:11 +0530

    conv

--- init.py
+++ init.py
@@ -22,8 +22,6 @@ layer_conv11, weights_conv11 = \
                    num_filters=num_filters11,
                    use_pooling=False)
 
-
-
 layer_conv12, weights_conv12 = \
     new_conv_layer(input=layer_conv11,
                    num_input_channels=num_filters11,
@@ -31,73 +29,73 @@ layer_conv12, weights_conv12 = \
                    num_filters=num_filters12,
                    use_pooling=True)
 
-layer_conv11, weights_conv11 = \
-    new_conv_layer(input=x_image,
-                   num_input_channels=num_channels,
-                   filter_size=filter_size11,
-                   num_filters=num_filters11,
+
+
+layer_conv21, weights_conv21 = \
+    new_conv_layer(input=layer_conv12,
+                   num_input_channels=num_filters12,
+                   filter_size=filter_size21,
+                   num_filters=num_filters21,
+                   use_pooling=False)
+
+layer_conv22, weights_conv22 = \
+    new_conv_layer(input=layer_conv21,
+                   num_input_channels=num_filters21,
+                   filter_size=filter_size22,
+                   num_filters=num_filters22,
+                   use_pooling=True)
+
+
+
+layer_conv31, weights_conv31 = \
+    new_conv_layer(input=layer_conv22,
+                   num_input_channels=num_filters22,
+                   filter_size=filter_size31,
+                   num_filters=num_filters31,
+                   use_pooling=False)
+
+layer_conv32, weights_conv32 = \
+    new_conv_layer(input=layer_conv31,
+                   num_input_channels=num_filters31,
+                   filter_size=filter_size32,
+                   num_filters=num_filters32,
+                   use_pooling=True)
+
+
+
+layer_conv41, weights_conv41 = \
+    new_conv_layer(input=layer_conv32,
+                   num_input_channels=num_filters32,
+                   filter_size=filter_size41,
+                   num_filters=num_filters41,
+                   use_pooling=False)
+
+layer_conv42, weights_conv42 = \
+    new_conv_layer(input=layer_conv41,
+                   num_input_channels=num_filters41,
+                   filter_size=filter_size42,
+                   num_filters=num_filters42,
+                   use_pooling=True)
+
+
+
+layer_conv51, weights_conv51 = \
+    new_conv_layer(input=layer_conv42,
+                   num_input_channels=num_filters42,
+                   filter_size=filter_size51,
+                   num_filters=num_filters51,
+                   use_pooling=False)
+
+layer_conv52, weights_conv52 = \
+    new_conv_layer(input=layer_conv51,
+                   num_input_channels=num_filters51,
+                   filter_size=filter_size52,
+                   num_filters=num_filters52,
                    use_pooling=False)
 
 
 
-layer_conv12, weights_conv12 = \
-    new_conv_layer(input=layer_conv11,
-                   num_input_channels=num_filters11,
-                   filter_size=filter_size12,
-                   num_filters=num_filters12,
-                   use_pooling=True)
-
-layer_conv11, weights_conv11 = \
-    new_conv_layer(input=x_image,
-                   num_input_channels=num_channels,
-                   filter_size=filter_size11,
-                   num_filters=num_filters11,
-                   use_pooling=False)
-
-
-
-layer_conv12, weights_conv12 = \
-    new_conv_layer(input=layer_conv11,
-                   num_input_channels=num_filters11,
-                   filter_size=filter_size12,
-                   num_filters=num_filters12,
-                   use_pooling=True)
-
-layer_conv11, weights_conv11 = \
-    new_conv_layer(input=x_image,
-                   num_input_channels=num_channels,
-                   filter_size=filter_size11,
-                   num_filters=num_filters11,
-                   use_pooling=False)
-
-
-
-layer_conv12, weights_conv12 = \
-    new_conv_layer(input=layer_conv11,
-                   num_input_channels=num_filters11,
-                   filter_size=filter_size12,
-                   num_filters=num_filters12,
-                   use_pooling=True)
-
-layer_conv11, weights_conv11 = \
-    new_conv_layer(input=x_image,
-                   num_input_channels=num_channels,
-                   filter_size=filter_size11,
-                   num_filters=num_filters11,
-                   use_pooling=False)
-
-
-
-layer_conv12, weights_conv12 = \
-    new_conv_layer(input=layer_conv11,
-                   num_input_channels=num_filters11,
-                   filter_size=filter_size12,
-                   num_filters=num_filters12,
-                   use_pooling=True)
-
-
-
-layer_flat, num_features = flatten_layer(layer_conv12)
+layer_flat, num_features = flatten_layer(layer_conv52)
 
 
 layer_fc1 = new_fc_layer(input=layer_flat,
